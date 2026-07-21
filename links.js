9674fdde13f0b5455fc32bbd0c9fcddd95d5fc49
kishanbasotia11-debug <kishanbasotia11@gmail.com>
2026-07-22 00:25:05 +0530

    Update links.js

--- links.js
+++ links.js
@@ -1,5 +1,7 @@
 const AFFILIATE_TAG = "dollargameryt-21";
 
 const products = [
+   "https://www.amazon.in/SNAPTRON-Handheld-Console-Portable-Rechargable/dp/B0G38K9N2S?pd_rd_w=sOAbc&content-id=amzn1.sym.f8d8b87f-14aa-4c03-ad0d-380a0108b532&pf_rd_p=f8d8b87f-14aa-4c03-ad0d-380a0108b532&pf_rd_r=1D236HDMW3WE9J85QY0V&pd_rd_wg=IFs9H&pd_rd_r=5bb94ab9-e0da-4c9f-9d79-6b7fb3e69594&pd_rd_i=B0G38K9N2S&th=1&linkCode=ll2&tag=dollargameryt-21&linkId=b990066dc045f09a6b732feb15bea3ee&ref_=as_li_ss_tl"'
+    
     "https://link.amazon/B02BzGvrX",
 ];
