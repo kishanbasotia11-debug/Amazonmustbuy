cbdbdc9b1495f3f21dffc4c82d7ba8048f1c02ca
kishanbasotia11-debug <kishanbasotia11@gmail.com>
2026-07-22 06:30:30 +0530

    Update links.js

--- links.js
+++ links.js
@@ -1,7 +1,7 @@
 const AFFILIATE_TAG = "dollargameryt-21";
 
 const products = [
-  "https://link.amazon/B0h26wSEl" .
-  "https://link.amazon/B097mKVTX" ,
-  "https://link.amazon/B0bl1KGF4" ,
+  "https://link.amazon/B0h26wSEl",
+  "https://link.amazon/B097mKVTX",
+  "https://link.amazon/B0bl1KGF4",
 ];
