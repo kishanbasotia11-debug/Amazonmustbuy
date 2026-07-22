f04f893bd2414ebf7a6bcb859970c3af0b1fcf27
kishanbasotia11-debug <kishanbasotia11@gmail.com>
2026-07-22 06:49:53 +0530

    Update links.js

--- links.js
+++ links.js
@@ -2,10 +2,10 @@ const AFFILIATE_TAG = "dollargameryt-21"; // amazon associate
 
 const products = [
 {
-title: "Product Name",
-price: "₹1,999",
-image: "https://your-image-url.jpg",
+title: "MD'S Home Insulated 860ml Tumbler with Handle and Straw Lid 100% Leak-Proof Cupholder Friendly Travel Mug 29oz Stainless Steel Insulated Tumbler with lid and straw Gifts for Women Men Him Her (PURPLE",
+price: "₹897",
+image: "https://m.media-amazon.com/images/I/71bsj9smvrL._SX679_.jpg",
 description: "Product description...",
-link: "https://link.amazon/B0h26wSEl"
+link: "https://link.amazon/B0i4xXzaZ"
 }
 ];
